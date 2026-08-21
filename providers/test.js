bee06701001a51a2b81416d90e2b4d71ca4922a4
aka3333 <adilkahraman89@gmail.com>
2026-08-22 01:39:31 +0300

    Update test.js

--- providers/test.js
+++ providers/test.js
@@ -1,87 +1,83 @@
 var TMDB_API_KEY = '500330721680edb6d5f7f12ba7cd9023';
-var VERSION = "8.0.7-STREMIO-FIX";
+var VERSION = "8.0.8-STREMIO-CALLBACK";
 
-function getStreams(tmdbId, mediaType, season, episode) {
-    return new Promise(function(resolve, reject) {
-        try {
-            var typePath = mediaType === 'movie' ? 'movie' : 'tv';
-            var tmdbUrl = 'https://api.themoviedb.org/3/' + typePath + '/' + tmdbId + '?api_key=' + TMDB_API_KEY + '&language=tr-TR&append_to_response=external_ids';
-            
-            // TMDB'den veri çek
-            fetch(tmdbUrl)
-                .then(function(tmdbRes) {
-                    if (!tmdbRes.ok) {
-                        resolve([]);
+function getStreams(tmdbId, mediaType, season, episode, callback) {
+    try {
+        var typePath = mediaType === 'movie' ? 'movie' : 'tv';
+        var tmdbUrl = 'https://api.themoviedb.org/3/' + typePath + '/' + tmdbId + '?api_key=' + TMDB_API_KEY + '&language=tr-TR&append_to_response=external_ids';
+        
+        fetch(tmdbUrl)
+            .then(function(tmdbRes) {
+                if (!tmdbRes.ok) {
+                    callback(null, []);
+                    return;
+                }
+                return tmdbRes.json();
+            })
+            .then(function(d) {
+                if (!d) {
+                    callback(null, []);
+                    return;
+                }
+                
+                var imdbId = d.external_ids && d.external_ids.imdb_id;
+                
+                if (!imdbId || imdbId.indexOf('tt') !== 0) {
+                    callback(null, []);
+                    return;
+                }
+
+                var title = d.title || d.name || "İçerik";
+                var targetUrl = '';
+                var displayTitle = title;
+
+                if (mediaType === 'movie') {
+                    targetUrl = 'https://vidmody.com/vs/' + imdbId;
+                    var year = d.release_date ? d.release_date.slice(0, 4) : '';
+                    if (year) displayTitle += ' (' + year + ')';
+                } else {
+                    if (!season || !episode) {
+                        callback(null, []);
                         return;
                     }
-                    return tmdbRes.json();
+                    var episodeStr = episode < 10 ? '0' + episode : '' + episode;
+                    targetUrl = 'https://vidmody.com/vs/' + imdbId + '/s' + season + '/e' + episodeStr;
+                    displayTitle += ' - S' + season + 'E' + episode;
+                }
+
+                fetch(targetUrl, { 
+                    method: 'HEAD',
+                    headers: {
+                        'Referer': 'https://vidmody.com/',
+                        'User-Agent': 'Mozilla/5.0'
+                    }
                 })
-                .then(function(d) {
-                    if (!d) {
-                        resolve([]);
-                        return;
-                    }
-                    
-                    var imdbId = d.external_ids && d.external_ids.imdb_id;
-                    
-                    if (!imdbId || imdbId.indexOf('tt') !== 0) {
-                        resolve([]);
-                        return;
-                    }
-
-                    var title = d.title || d.name || "İçerik";
-                    var targetUrl = '';
-                    var displayTitle = title;
-
-                    if (mediaType === 'movie') {
-                        targetUrl = 'https://vidmody.com/vs/' + imdbId;
-                        var year = d.release_date ? d.release_date.slice(0, 4) : '';
-                        if (year) displayTitle += ' (' + year + ')';
+                .then(function(checkRes) {
+                    if (checkRes.ok) {
+                        callback(null, [{
+                            url: targetUrl,
+                            name: 'Vidmody',
+                            title: displayTitle,
+                            quality: 'Auto',
+                            headers: {
+                                'Referer': 'https://vidmody.com/',
+                                'User-Agent': 'Mozilla/5.0'
+                            }
+                        }]);
                     } else {
-                        if (!season || !episode) {
-                            resolve([]);
-                            return;
-                        }
-                        var episodeStr = episode < 10 ? '0' + episode : '' + episode;
-                        targetUrl = 'https://vidmody.com/vs/' + imdbId + '/s' + season + '/e' + episodeStr;
-                        displayTitle += ' - S' + season + 'E' + episode;
+                        callback(null, []);
                     }
-
-                    // Link kontrolü
-                    fetch(targetUrl, { 
-                        method: 'HEAD',
-                        headers: {
-                            'Referer': 'https://vidmody.com/',
-                            'User-Agent': 'Mozilla/5.0'
-                        }
-                    })
-                    .then(function(checkRes) {
-                        if (checkRes.ok) {
-                            resolve([{
-                                url: targetUrl,
-                                name: 'Vidmody',
-                                title: displayTitle,
-                                quality: 'Auto',
-                                headers: {
-                                    'Referer': 'https://vidmody.com/',
-                                    'User-Agent': 'Mozilla/5.0'
-                                }
-                            }]);
-                        } else {
-                            resolve([]);
-                        }
-                    })
-                    .catch(function() {
-                        resolve([]);
-                    });
                 })
                 .catch(function() {
-                    resolve([]);
+                    callback(null, []);
                 });
-        } catch (e) {
-            resolve([]);
-        }
-    });
+            })
+            .catch(function() {
+                callback(null, []);
+            });
+    } catch (e) {
+        callback(null, []);
+    }
 }
 
 if (typeof module !== 'undefined') module.exports = { getStreams: getStreams };
